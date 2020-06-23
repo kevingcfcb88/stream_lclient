79bfbff816c397daf0ab181a65f69ae2cc572a55
Kevin Garay <kevingcfcb88@gmail.com>
2020-06-22 21:51:54 -0600

    Stream working

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
 
@@ -28,7 +28,9 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>          
         </div>
